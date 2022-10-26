ea98d837a1af8829f168e2442120f981edca3f0f
Abhishek <shindeaa21.comp@coep.ac.in>
2022-10-26 23:56:29 +0530

    added popup

---
+++ poppup.js
@@ -0,0 +1,16 @@
+function togglePopup(){
+    // var blur = document.getElementById('drama');
+    // blur.classList.toggle('active');
+    // var blur = document.getElementById('dance');
+    // blur.classList.toggle('active');
+    // var blur = document.getElementById('music');
+    // blur.classList.toggle('active');
+    // var blur = document.getElementById('anc');
+    // blur.classList.toggle('active');
+    var blur = document.getElementById('blur1');
+    blur.classList.toggle('active');
+    var blur = document.getElementById('blur2');
+    blur.classList.toggle('active');
+    var popup = document.getElementById('popup');
+    popup.classList.toggle('active')
+}
