467c50104ece9f326fe7c9d1da19b79fdeceec99
Abhishek <shindeaa21.comp@coep.ac.in>
2022-10-27 20:35:48 +0530

    jquery changes

--- jsFiles/navBar.js
+++ jsFiles/navBar.js
@@ -10,68 +10,71 @@ const allItems = document.querySelectorAll(".leftContainer ul li");
 //     })
 // })
 
-$(window).scroll(function () {
+$(document).ready(function () {
+    $(window).scroll(function () {
 
-    var offsetTopPosition1 = $("#dance").offset().top - $(window).scrollTop();
-    var offsetTopPosition2 = $("#drama").offset().top - $(window).scrollTop();
-    var offsetTopPosition3 = $("#anc").offset().top - $(window).scrollTop();
-    var offsetTopPosition4 = $("#camera").offset().top - $(window).scrollTop();
-    var offsetTopPosition5 = $("#music").offset().top - $(window).scrollTop();
-    var offsetTopPosition6 = $("#shoutout").offset().top - $(window).scrollTop();
+        var offsetTopPosition1 = $("#dance").offset().top - $(window).scrollTop();
+        var offsetTopPosition2 = $("#drama").offset().top - $(window).scrollTop();
+        var offsetTopPosition3 = $("#anc").offset().top - $(window).scrollTop();
+        var offsetTopPosition4 = $("#camera").offset().top - $(window).scrollTop();
+        var offsetTopPosition5 = $("#music").offset().top - $(window).scrollTop();
+        var offsetTopPosition6 = $("#shoutout").offset().top - $(window).scrollTop();
 
-    if (offsetTopPosition1 < 500) {
-        for (var i = 0; i < allItems.length; i++) {
-            allItems[i].classList.remove("active");
+        if (offsetTopPosition1 < 500) {
+            for (var i = 0; i < allItems.length; i++) {
+                allItems[i].classList.remove("active");
+            }
+            $(".nitem1").addClass('active');
         }
-        $(".nitem1").addClass('active');
-    }
-    else{
-        $(".nitem1").removeClass('active');
-    }
-    if (offsetTopPosition2 < 500) {
-        for (var i = 0; i < allItems.length; i++) {
-            allItems[i].classList.remove("active");
+        else {
+            $(".nitem1").removeClass('active');
         }
-        $(".nitem2").addClass('active');
-    }
-    else{
-        $(".nitem2").removeClass('active');
-    }
-    if (offsetTopPosition3 < 500) {
-        for (var i = 0; i < allItems.length; i++) {
-            allItems[i].classList.remove("active");
+        if (offsetTopPosition2 < 500) {
+            for (var i = 0; i < allItems.length; i++) {
+                allItems[i].classList.remove("active");
+            }
+            $(".nitem2").addClass('active');
         }
-        $(".nitem3").addClass('active');
-    }
-    else{
-        $(".nitem3").removeClass('active');
-    }
-    if (offsetTopPosition4 < 500) {
-        for (var i = 0; i < allItems.length; i++) {
-            allItems[i].classList.remove("active");
+        else {
+            $(".nitem2").removeClass('active');
         }
-        $(".nitem4").addClass('active');
-    }
-    else{
-        $(".nitem4").removeClass('active');
-    }
-    if (offsetTopPosition5 < 500) {
-        for (var i = 0; i < allItems.length; i++) {
-            allItems[i].classList.remove("active");
+        if (offsetTopPosition3 < 500) {
+            for (var i = 0; i < allItems.length; i++) {
+                allItems[i].classList.remove("active");
+            }
+            $(".nitem3").addClass('active');
         }
-        $(".nitem5").addClass('active');
-    }
-    else{
-        $(".nitem5").removeClass('active');
-    }
-    if (offsetTopPosition6 < 500) {
-        for (var i = 0; i < allItems.length; i++) {
-            allItems[i].classList.remove("active");
+        else {
+            $(".nitem3").removeClass('active');
         }
-        $(".nitem6").addClass('active');
-    }
-    else{
-        $(".nitem6").removeClass('active');
-    }
+        if (offsetTopPosition4 < 500) {
+            for (var i = 0; i < allItems.length; i++) {
+                allItems[i].classList.remove("active");
+            }
+            $(".nitem4").addClass('active');
+        }
+        else {
+            $(".nitem4").removeClass('active');
+        }
+        if (offsetTopPosition5 < 500) {
+            for (var i = 0; i < allItems.length; i++) {
+                allItems[i].classList.remove("active");
+            }
+            $(".nitem5").addClass('active');
+        }
+        else {
+            $(".nitem5").removeClass('active');
+        }
+        if (offsetTopPosition6 < 500) {
+            for (var i = 0; i < allItems.length; i++) {
+                allItems[i].classList.remove("active");
+            }
+            $(".nitem6").addClass('active');
+        }
+        else {
+            $(".nitem6").removeClass('active');
+        }
+
+    });
 
 });
