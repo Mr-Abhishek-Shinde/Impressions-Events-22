57d8b0724cae851ba9d1fbd11e64aad6d2e7dc33
Abhishek <shindeaa21.comp@coep.ac.in>
2022-10-27 17:27:25 +0530

    added event name in popup

--- jsFiles/poppup.js
+++ jsFiles/poppup.js
@@ -1,16 +1,42 @@
-function togglePopup(){
-    // var blur = document.getElementById('drama');
-    // blur.classList.toggle('active');
-    // var blur = document.getElementById('dance');
-    // blur.classList.toggle('active');
-    // var blur = document.getElementById('music');
-    // blur.classList.toggle('active');
-    // var blur = document.getElementById('anc');
-    // blur.classList.toggle('active');
+function togglePopup(id) {
     var blur = document.getElementById('blur1');
     blur.classList.toggle('active');
     var blur = document.getElementById('blur2');
     blur.classList.toggle('active');
     var popup = document.getElementById('popup');
     popup.classList.toggle('active')
+
+
+
+    var EveName = document.getElementById("EveName")
+
+
+    switch (id) {
+        case "SF":
+            EveName.innerHTML = "Street Forward";
+            break;
+
+        case "SD":
+            EveName.innerHTML = "So-Duet";
+            break;
+
+        case "SW":
+            EveName.innerHTML = "Swa(g)Desi";
+            break;
+        case "NG":
+            EveName.innerHTML = "Nrityangana";
+            break;
+        case "LL":
+            EveName.innerHTML = "Limelight";
+            break;
+        case "AS":
+            EveName.innerHTML = "Artiskit";
+            break;
+        case "SS":
+            EveName.innerHTML = "Suno Suno";
+            break;
+        default:
+            EveName.innerHTML = "Event Name";
+            break;
+    }
 }
